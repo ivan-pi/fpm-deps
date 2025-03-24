944fa36534523ae294a9919a036d69b2e7c0ac8f
Ivan Pribec <ivan.pribec@gmail.com>
2025-03-24 22:06:57 +0100

    Some notes on UTF-8 detection

--- app/fpm-tree/fpm-tree.f90
+++ app/fpm-tree/fpm-tree.f90
@@ -5,7 +5,7 @@ use, intrinsic :: iso_fortran_env, only: error_unit, output_unit
 
 use fpm_deps, only: config_t, tree_t, new_tree, &
     package_properties, dependency_props, &
-    exclude_mask, print_deps, bfs_depth
+    exclude_mask, print_deps, dependency_depth
 use fpm_error, only: error_t
 
 implicit none
@@ -19,7 +19,7 @@ type(package_properties), allocatable :: props(:)
 character(len=:), allocatable :: manifest_path
 
 integer :: cmd_depth
-logical :: cmd_deduplicate
+logical :: cmd_deduplicate, cmd_utf8
 
 integer :: debug_unit, nargs, blen, k
 character(len=1024) :: buf
@@ -34,6 +34,7 @@ name = buf(1:blen)
 cmd_depth = -1
 cmd_deduplicate = .true.
 
+cmd_utf8 = .true.
 
 ! Process command-line arguments
 k = 1
@@ -120,5 +121,47 @@ contains
 "  https://github.com/ivan-pi/fpm-deps"
 
     end subroutine
+
+
+    function likely_supports_utf8() result(utf8)
+
+! Do modern terminals generally render all utf-8 characters correctly?
+! https://stackoverflow.com/questions/32928589/do-modern-terminals-generally-render-all-utf-8-characters-correctly?noredirect=1&lq=1
+
+! Reasonably determine whether terminal can correctly render UTF-8 characters in C?
+! https://stackoverflow.com/questions/77621760/reasonably-determine-whether-terminal-can-correctly-render-utf-8-characters-in-c
+
+! How to find out if a terminal supports UTF-8
+! https://serverfault.com/questions/13898/how-to-find-out-if-a-terminal-supports-utf-8
+
+! Determine if the term/console supports UTF8?
+! https://www.reddit.com/r/bash/comments/wfbf3w/determine_if_the_termconsole_supports_utf8/
+
+! Detect how much of Unicode my terminal supports, even through screen
+! https://unix.stackexchange.com/questions/184345/detect-how-much-of-unicode-my-terminal-supports-even-through-screen
+
+! Is there a way to determine Unicode support status in a terminal emulator?
+! https://www.reddit.com/r/commandline/comments/vrflbt/is_there_a_way_to_determine_unicode_support/
+
+        character(len=80) :: str
+        integer :: strlen, stat
+        logical :: utf8
+
+        call get_environment_variable('LC_ALL',str,strlen,status=stat)
+        if (stat /= 0) call get_environment_variable('LC_CYTPE',str,strlen,status=stat)
+        if (stat /= 0) call get_environment_variable('LANG',str,strlen,status=stat)
+
+        utf8 = index(str(1:strlen),'UTF-8') > 0
+
+        call get_environment_variable('TERM',str,strlen,status=stat)
+        if (stat == 0) then
+            select case(str(1:strlen))
+            case('xterm','xterm-256color')
+                utf8 = .true.
+            case default
+                utf8 = .false.
+            end select
+        end if
+    end function
 
 end program fpm_tree_main
